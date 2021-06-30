8ebba07f3ecee8a3b70f7bc1741e7e514459fc1d
Luisa Fernanda Bolaños Egas <lbolanos@everis.com>
2021-06-30 08:32:25 -0500

    mejora codigo

---
+++ src/main/java/aws/example/s3/Constants.java
@@ -0,0 +1,13 @@
+package aws.example.s3;
+
+public final class ConstantsExample {
+
+    private ConstantsExample() {
+    }
+
+    public static final String SQS_PARAMETER = "/pruebaParameter/fifo";
+
+    public static final String BUCKET_PARAMETER = "/pruebaParameter/bucket";
+
+    public static final String ROUTE_DOWNLOAD = "C:/Users/lbolanos/Documents/Download/";
+}
